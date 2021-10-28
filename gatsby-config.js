f014c17ce9fcde3435314c6d04976191a1c04764
Leo Ware <leoware@minerva.kgi.edu>
2021-10-28 13:06:02 +0200

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Cool capstoney thing`,
+    author: `Leo Ware`,
+    // description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `gatsbyjs`,
